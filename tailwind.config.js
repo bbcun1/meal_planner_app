e0cb90b93d1dc9b656ee2d281ad4222da9e0ff49
Barry Cunningham <barry.cunningham7@gmail.com>
2025-05-25 17:42:19 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,7 @@
-# Tailwind config
-cat > tailwind.config.js << 'EOL'
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
-EOL
-
-# PostCSS config
-cat > postcss.config.js << 'EOL'
-module.exports = {
-  plugins: { tailwindcss: {}, autoprefixer: {} },
-}
-EOL
-
-# Vite config
-cat > vite.config.ts << 'EOL'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({ plugins: [react()] })
-EOL
